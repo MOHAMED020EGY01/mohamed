55d24d7ff6848fc6add660777a3f4b3b7eb4a794
mohamed020egy0 <mohamed02051010@gmail.com>
2024-08-29 08:36:01 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let img = document.getElementById('img');
+let colo = document.getElementById('headers')
+
+function phone(change_phone){
+    img.src = change_phone;
+}
+
+function color(chang_bg){
+    colo.style.background = chang_bg ;
+}
+
